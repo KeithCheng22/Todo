344a82d7e9cbc7b07faa8777453d4eb85bc631be
KeithCheng22 <keithcheng.sgs@gmail.com>
2024-02-17 12:17:48 +0800

    fix db

--- utils/database.js
+++ utils/database.js
@@ -11,7 +11,10 @@ export const connectToDB = async () => {
 
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: 'myFirstDatabase'
+            dbName: 'myFirstDatabase',
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+            
         })
         isConnected = true;
         if (isConnected) console.log("DB Connected!")
